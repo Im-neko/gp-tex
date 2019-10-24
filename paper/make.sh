b760a7871cc9ed75fcbd6d55188e7d64a5bd45b1
im-neko <yuki@ideta.net>
2019-10-24 18:45:40 +0900

    update

--- paper/make.sh
+++ paper/make.sh
@@ -1,2 +1,2 @@
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex resume.tex
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx resume.dvi
+docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja uplatex thesis.tex
+docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja dvipdfmx thesis.dvi
