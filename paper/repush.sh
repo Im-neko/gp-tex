8b4cb4430ea0b1f169ba6e2884288502b2410385
im-neko <yuki@ideta.net>
2019-12-12 16:49:11 +0900

    bib

--- paper/repush.sh
+++ paper/repush.sh
@@ -17,9 +17,10 @@ echo $last_commit_message
 echo $PWD
 
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
-sleep 1s
+docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest pbibtex thesis
 docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
-docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest dvipdfmx thesis.dvi
+docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest platex thesis.tex
+docker run --rm -v $PWD:/workdir paperist/alpine-texlive-ja:latest dvipdfmx thesis
 
 echo '!paper/thesis.pdf' >> ../.gitignore
 git add .
